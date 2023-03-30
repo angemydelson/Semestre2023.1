e18771c101728efabda68335823e2e385d992e7d
Angemydelson SAINT-BERT <delsonysuperdel@gmail.com>
2023-03-30 11:08:48 -0300

    slides/img

---
+++ Grafos/Exercises/tarefa_00_a_solucao/somador.cpp
@@ -0,0 +1,26 @@
+/*
+ * Tarefa de laboratorio 00 - Somador
+ *
+ * GEN254 - Grafos - 2023/1
+ *
+ * Nome:      XXXX
+ * Matricula: XXXX
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    string c;
+
+    cin >> a >> b >> c;
+
+    cout << (a + b) << "\n";
+    cout << (c) << "\n";
+
+    return 0;
+}
