e3ace0496b38ed551a3e208dd59c8e1eadae1c1f
Angemydelson SAINT-BERT <delsonysuperdel@gmail.com>
2023-05-04 04:20:19 -0300

    Atualizações

--- Programa de Monitoria/Lista 2/1329.py
+++ Programa de Monitoria/Lista 2/1329.py
@@ -1,17 +1,15 @@
-
 while True:
     N = int(input())
-    i = 0
     caraM = 0
     coroaJ = 0
     if N == 0:
         break
-    while i < N:
-        entrada = int(input())
+    entradas = input().split()
+    for i in range(N):
+        entrada = int(entradas[i])
         if entrada == 0:
             caraM += 1
         else :
             if entrada == 1:
                 coroaJ += 1
-        i += 1
     print(f"Mary won {caraM} times and John won {coroaJ} times")
